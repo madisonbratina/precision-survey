8ce7e069a3e230964ec5a4f83f732a8c0234ba9f
Js_developer <js.developer@ongraph.com>
2024-03-11 19:24:00 +0530

    CouponApi-bugFix

--- src/app/api/coupon/providers/route.ts
+++ src/app/api/coupon/providers/route.ts
@@ -1,47 +1,31 @@
-import ChipList from '@/components/Common/ChipList';
 import { NextResponse } from 'next/server';
 import dbConnect from '~/db/db';
 import Coupon from '~/models/coupon';
+export const revalidate = 0;
 
-function getUniqueEntriesWithProperties(data: any) {
-  console.log('Starting getUniqueEntriesWithProperties function');
-  const uniqueProviders: any = {};
-  const result = [];
-  console.log('Data received:', data);
+async function getUniqueEntriesWithProperties() {
+  try {
+    const [timHortonsCoupon, amazonCoupon] = await Promise.all([
+      Coupon.findOne({ provider: 'tim hortons', userId: { $exists: false } }),
+      Coupon.findOne({ provider: 'amazon', userId: { $exists: false } })
+    ]);
 
-  for (let i = 0; i < data.length; i++) {
-    const entry = data[i];
-    console.log(`Processing entry ${i}:`, entry);
-    if (entry.provider === 'tim hortons' || entry.provider === 'amazon') {
-      console.log(`Entry provider is ${entry.provider}`);
-      if (!(entry.provider in uniqueProviders)) {
-        console.log(`Adding provider ${entry.provider} to uniqueProviders`);
-        uniqueProviders[entry.provider] = true;
-        const { _id, code, provider, image } = entry;
-        result.push({ _id, code, provider, image: image || null });
-        console.log(uniqueProviders, 'uniqueProviders');
-        console.log(result, 'result');
-      }
+    // Simplify conditional logic
+    const foundCoupons = [timHortonsCoupon, amazonCoupon].filter(Boolean);
+    if (foundCoupons.length > 0) {
+      return foundCoupons;
+    } else {
+      return [];
     }
+  } catch (error) {
+    return [];
   }
-
-  return result;
 }
 
 export async function GET() {
   try {
-    // await dbConnect();
-    // const response = await Coupon.find({ userId: { $exists: false } });
-    // const result = getUniqueEntriesWithProperties(response);
-
-    console.log('Starting GET function');
     await dbConnect();
-    const response = await Coupon.find({ userId: { $exists: false } });
-    console.log('Response from database:', response);
-    const result = getUniqueEntriesWithProperties(response);
-    console.log('Result from getUniqueEntriesWithProperties:', result);
-    // console.log(response, 'response in the coupon/provider');
-    // console.log(result, 'result  in the coupon/provider');
+    const result = await getUniqueEntriesWithProperties();
     return NextResponse.json({ data: result }, { status: 200 });
   } catch (e) {
     return NextResponse.json(e, { status: 500 });
