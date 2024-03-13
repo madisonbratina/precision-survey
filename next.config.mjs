c64f67a09fde35b691536890a8182746a8eb20c9
madisonbratina <madisonbratina@gmail.com>
2024-03-13 17:18:50 -0400

    add maintenance mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+// next.config.mjs
+const isMaintenanceMode = process.env.MAINTENANCE_MODE === 'ON';
+
+export default {
+  async redirects() {
+    if (isMaintenanceMode) {
+      return [
+        {
+          source: '/((?!maintenance).*)',
+          destination: '/maintenance',
+          permanent: false,
+        },
+      ];
+    } else {
+      return nextConfig;
+    }
+  },
+};
---
+++ src/app/maintenance.page.tsx
@@ -0,0 +1,14 @@
+// app/maintenance.page.tsx
+
+import React from 'react';
+
+const Maintenance: React.FC = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', textAlign: 'center' }}>
+      <h1>We're Currently Under Maintenance</h1>
+      <p>Sorry for the inconvenience. We're performing some maintenance at the moment. Please check back later.</p>
+    </div>
+  );
+};
+
+export default Maintenance;
